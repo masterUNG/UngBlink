9744bc022bb40cf64eaccc3895df4347ae8c8945
MasterUNG <phrombutr@gmail.com>
2018-01-24 11:55:23 +0700

    finish

--- UngBlink.ino
+++ UngBlink.ino
@@ -1,6 +1,8 @@
 //ส่วนของการประกาศตัวแปร
-int myOutput = D0;
-int myDelayTime = 2000; //ค่าของเวลาจะมีหน่วยเป็น มิลิวินาที
+int myOutput = D7;
+int myOutput2 = D8;
+
+int myDelayTime = 3000; //ค่าของเวลาจะมีหน่วยเป็น มิลิวินาที
 
 
 
@@ -10,6 +12,7 @@ void setup() {
 
 //  ส่วนที่กำหนด การส่ง Digital Out จากขาของ Node
   pinMode(myOutput, OUTPUT);
+  pinMode(myOutput2, OUTPUT);
 
 
   
@@ -17,8 +20,24 @@ void setup() {
 
 //ส่วนของ loop จะทำงาน วนไปมา ตาม Digital Logic
 void loop() {
+
+//  for myOutput
   digitalWrite(myOutput, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(myDelayTime);                       // wait for a second
   digitalWrite(myOutput, LOW);    // turn the LED off by making the voltage LOW
   delay(myDelayTime);                       // wait for a second
-}
+
+//  for myOutput2
+  digitalWrite(myOutput2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(myDelayTime);                       // wait for a second
+  digitalWrite(myOutput2, LOW);    // turn the LED off by making the voltage LOW
+  delay(myDelayTime);                       // wait for a second
+
+  
+} // end of Loop
+
+
+
+
+
+
